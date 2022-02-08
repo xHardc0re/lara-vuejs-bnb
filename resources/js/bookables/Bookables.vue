9083f7c4427ef12b03b95b1da8b5ba8069e3f07c
xhardc0re <xhardcqre@gmail.com>
2022-02-08 22:05:52 +0200

    L18

---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <bookable-list-item></bookable-list-item>
+    </div>
+</template>
+
+<script>
+import BookableListItem from "./BookableListItem";
+
+export default {
+    components: {
+        BookableListItem
+    }
+}
+</script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,11 +1,11 @@
 import VueRouter from "vue-router";
-import ExampleComponent from "./components/ExampleComponent";
+import Bookables from "./bookables/Bookables";
 import Example2 from "./components/Example2";
 
 const routes = [
     {
         path: "/",
-        component: ExampleComponent,
+        component: Bookables,
         name: "home"
     },
     {
